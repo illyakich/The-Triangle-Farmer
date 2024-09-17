43e9643f5ea020cca6127869c800165c4dd15d2d
ПК <@DESKTOP-5UNSHBK>
2024-09-17 19:48:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Whats the base size of the triangle?");
+float Base = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("now, whats the height of the triangle?");
+float height  = Convert.ToSingle(Console.ReadLine());
+
+float Area = (Base * height) / 2;
+
+Console.WriteLine($"the Area of the triange is {Area} ");
